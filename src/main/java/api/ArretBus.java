6bd171bc5d50e6075d307229d3e088ccdc4c6749
Mamadi KABA <madkabx@gmail.com>
2015-01-13 14:37:59 +0100

    Parsing du resultat getbusnextdeparture

--- src/main/java/api/ArretBus.java
+++ src/main/java/api/ArretBus.java
@@ -1,26 +1,22 @@
 package api;
 
-import java.awt.Paint;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Iterator;
 
 public class ArretBus implements Requete {
 	private String version = "2.2";
 	private String command;
-	private Map<String, String> params;
 	private Collection<Parametre> paramtres;
 	public ArretBus(String version, String command) {
 		this.version = version;
 		this.command = command;
 		this.paramtres = new ArrayList<Parametre>();
 	}
-
+	
 
 	public String getCommand() {
-
-		return null;
+		return this.command;
 	}
 
 	public String getVersion() {
@@ -28,24 +24,25 @@ public class ArretBus implements Requete {
 	}
 
 	public String getParametre() {
-		//		if(this.params.size()!=0)
+		String params = "";
+		if(this.paramtres.size()!=0){
+			Iterator<Parametre> itParametre = this.paramtres.iterator();
+			while (itParametre.hasNext()){
+				params += "&param["+ itParametre.next().getKey()+"]="+ itParametre.next().getValue();
+			}
+			return params;	
+		}
 		return null;
 	}
 
+	public String getRequete() {
+		return "?cmd="+ this.getCommand()+ "" + "&version="+ this.getVersion() + this.getParametre();
+	}
 
 	public void addParametre(String key, String value){
-
-		params.put(key, value);
+		this.paramtres.add(new Parametre(key, value));
 	}
 	public boolean removeParam(String key){
-		if(this.params.containsKey(key)){
-			this.params.remove(key);
-			return true;
+		return true;
 		}
-		return false;
 	}
-	public String getRequete() {
-		return null;
-	}
-
-}
--- src/main/java/api/ClientREST.java
+++ src/main/java/api/ClientREST.java
@@ -1,11 +1,24 @@
 package api;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ClientREST {
 	private String key;
 	private URI domain;
 	private String format ="json" ;
+	private Requete requete;
 	
 	public ClientREST(String key, URI host, String format) {
 		this.key = key;
@@ -30,6 +43,30 @@ public class ClientREST {
 	}
 	public void setFormat(String format) {
 		this.format = format;
+	}
+	public Requete getRequete() {
+		return requete;
+	}
+	public void setRequete(Requete requete) {
+		this.requete = requete;
+	}
+	public  JSONObject execute() throws URISyntaxException, HttpException, IOException{
+	
+		String requeteHTTP = "";
+		HttpClient client = new DefaultHttpClient();
+		requeteHTTP = getDomain()+ getFormat()+ "/?"+ getKey() + requete.getRequete();
+		HttpGet request = new HttpGet(requeteHTTP);
+		HttpResponse response = client.execute(request);
+		 BufferedReader br =  new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
+
+			JSONObject status  = new JSONObject(br.readLine());
+			JSONObject openData = status;
+			int code = status.getJSONObject("opendata").getJSONObject("answer").getJSONObject("status").getJSONObject("@attributes").getInt("code");
+			if ( code==0) {
+				
+				return  openData.getJSONObject("opendata").getJSONObject("answer").getJSONObject("data");
+			}
+			return null;
 	}
 	
 	
