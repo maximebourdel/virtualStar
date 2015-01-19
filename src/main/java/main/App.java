200ac721dd060e787fdd7c895726b1d50fc12af7
jeje <metamarron@gmail.com>
2015-01-19 17:08:19 +0100

    simplification de la classe de "parsage" pour éviter de passer par l'objet + "pretty printation" de la base mongodb + prise en compte du cas sans bus dans l'heure by salim et j-e

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -8,28 +8,30 @@ import mongoDB.ClientMongoDB;
 
 import org.apache.http.HttpException;
 
+import api.ClientREST;
+import api.RequeteArretBus;
+
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
-
-import api.ClientREST;
-import api.RequeteArretBus;
 
 public class App {
 
 	public static void main(String[] args) throws URISyntaxException, HttpException, IOException {
 		RequeteArretBus requeteArretBus = new RequeteArretBus();
 		requeteArretBus.addParametre("mode","stopline");
-		requeteArretBus.addParametre("route][","0001");
+		requeteArretBus.addParametre("route][","0004");
 		requeteArretBus.addParametre("direction][","0");
-		requeteArretBus.addParametre("stop][","1005");
+		requeteArretBus.addParametre("stop][","1160");
 
 		ClientREST clientREST = new ClientREST();
 		clientREST.setRequete(requeteArretBus);
 		ClientMongoDB clientMongoDB = ClientMongoDB.getInstance();
 		clientMongoDB.setDB("star");
-		
+
 		MongoClient mongoClient = clientMongoDB.getMongoClient();
 		DB dataBase = clientMongoDB.getDB();
 
@@ -40,6 +42,13 @@ public class App {
 			System.out.println(basicDBObject);
 		}
 		else System.out.println("null");
+		DBCursor cursor = collection.find();
+		while(cursor.hasNext()) {
+			DBObject object = cursor.next();
+			System.out.println("****************************************");
+			System.out.println("\n id arret : " + object.get("id_arret") + "\n id ligne : " + object.get("id_ligne") + "\n date requete : " + object.get("date_requete") + "\n direction : " + object.get("direction") + "\n en tête : " + object.get("en_tete") + "\n prevu : " + object.get("prevu") + "\n reel : " + object.get("reel") + "\n num vehic : " + object.get("num_vehicule") + "\n precision : " + object.get("precision"));
+		}
+		cursor.close();
 		clientMongoDB.close();
 
 
--- src/main/java/mongoDB/ArretBusParser.java
+++ src/main/java/mongoDB/ArretBusParser.java
@@ -5,6 +5,7 @@ import metier.ArretBus;
 import metier.Ligne;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import util.ArretBusAttribut;
@@ -14,38 +15,42 @@ import com.mongodb.BasicDBObject;
 public class ArretBusParser {
 
 	public static BasicDBObject parser(JSONObject jsonObject) {
-		ArretBus arretBus = new ArretBus();
-		Arret arret =  new Arret();
-		arret.setId(jsonObject.getJSONObject("stopline").getString("stop"));
-		arretBus.setArret(arret);
-		arretBus.setDate_requete(jsonObject.getJSONObject("@attributes").getString("localdatetime"));
-		arretBus.setDirection(jsonObject.getJSONObject("stopline").getString("direction"));
-		Ligne ligne = new Ligne();
-		ligne.setId(jsonObject.getJSONObject("stopline").getString("route"));
-		arretBus.setLigne(ligne);
 
-		JSONObject  json = jsonObject.getJSONObject("stopline");
-		JSONObject departures =json.getJSONObject("departures");
-		JSONObject value ;
+		JSONObject json;
+		JSONObject departures;
+		JSONArray departure;
+		JSONObject value;
 		
-		try {
-			JSONArray departure = departures.getJSONArray("departure");
-			value =departure.getJSONObject(0);
-		} catch (Exception e) {
-			value = departures.getJSONObject("departure");
+		try{
+			json = jsonObject.getJSONObject("stopline");
+			departures = json.getJSONObject("departures");
+			departure = departures.getJSONArray("departure");
+			value = departure.getJSONObject(0);
 		}
-
-		if(value==null){
-			//aucune donnee 
+		catch (JSONException je){
 			return null;
 		}
+
+
+		//		try{	
 		
+		//		} catch (Exception e) {
+		//			
+		//			value = departures.getJSONObject("departure");
+		//			System.out.println("catch " + value);
+		//		}
+		//		
+		//		if(value==null){
+		//			//aucune donnee 
+		//			return null;
+		//		}
+
 		BasicDBObject arretBusMongo = new BasicDBObject();
-		arretBusMongo.append("id_arret",arretBus.getArret().getId());
-		arretBusMongo.append("id_ligne",arretBus.getLigne().getId());
-		arretBusMongo.append(ArretBusAttribut.DATE_REQUETE, arretBus.getDate_requete());
-		arretBusMongo.append(ArretBusAttribut.DIRECTION, arretBus.getDirection());
-		arretBusMongo.append(ArretBusAttribut.EN_TETE, arretBus.getEn_tete());
+		arretBusMongo.append("id_arret",jsonObject.getJSONObject("stopline").getString("stop"));
+		arretBusMongo.append("id_ligne",jsonObject.getJSONObject("stopline").getString("route"));
+		arretBusMongo.append(ArretBusAttribut.DATE_REQUETE, jsonObject.getJSONObject("@attributes").getString("localdatetime"));
+		arretBusMongo.append(ArretBusAttribut.DIRECTION, jsonObject.getJSONObject("stopline").getString("direction"));
+		arretBusMongo.append(ArretBusAttribut.EN_TETE, value.getJSONObject("@attributes").getString("headsign"));
 		arretBusMongo.append(ArretBusAttribut.NUM_VEHICULE, value.getJSONObject("@attributes").getString("vehicle"));
 		arretBusMongo.append(ArretBusAttribut.PRECISION, value.getJSONObject("@attributes").getString("accurate"));
 		arretBusMongo.append(ArretBusAttribut.PREVU, value.getJSONObject("@attributes").getString("expected"));
@@ -53,7 +58,7 @@ public class ArretBusParser {
 
 		return arretBusMongo;
 
-
+		
 	}
 
 }
