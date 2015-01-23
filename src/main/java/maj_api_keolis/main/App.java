4fe9d5ed06053a3988b8371ae169f4583ae3da6a
maximebourdel <bourdel.maxime.pro@gmail.com>
2015-01-23 09:36:27 +0100

    Ajout de la verification mettant a jour les donnees deja inserees

--- src/main/java/maj_api_keolis/main/App.java
+++ src/main/java/maj_api_keolis/main/App.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 
 import maj_api_keolis.mongoDB.ArretBusParser;
 import maj_api_keolis.mongoDB.ClientMongoDB;
+import maj_api_keolis.util.ArretBusAttribut;
 
 import org.apache.http.HttpException;
 
@@ -37,11 +38,39 @@ public class App {
 
 		DBCollection collection = dataBase.getCollection("arretbus");
 		BasicDBObject basicDBObject = ArretBusParser.parser(clientREST.execute());
+		
+		
+		//On construit ici un objet ne contenant que les donnes : "prevu" et "vehicule" (nos attributs a tester)
+		DBObject searchIfArretBusAlreadyExist = collection.findOne(
+				new BasicDBObject()
+				.append(ArretBusAttribut.PREVU, basicDBObject.get(ArretBusAttribut.PREVU))//on vérifie la date
+				.append(ArretBusAttribut.NUM_VEHICULE, basicDBObject.get(ArretBusAttribut.NUM_VEHICULE))//on vérifie le numéro du vehicule
+		);
+		
+		//si on a bien parse notre objet et qu'il n'existe pas deja, on l'insere
 		if(basicDBObject!=null){
-			collection.insert(basicDBObject);
-			System.out.println(basicDBObject);
-		}
-		else System.out.println("null");
+			//
+			if (searchIfArretBusAlreadyExist != null) {
+				
+				//variable temporaire qui représentera notre nouvelle ligne a update
+				BasicDBObject UpdatedBasicDBObject = basicDBObject;
+				
+				//modification des nouvelles donnees
+				UpdatedBasicDBObject.put(ArretBusAttribut.DATE_REQUETE,basicDBObject.get(ArretBusAttribut.DATE_REQUETE));
+				UpdatedBasicDBObject.put(ArretBusAttribut.REEL,basicDBObject.get(ArretBusAttribut.REEL));
+				UpdatedBasicDBObject.put(ArretBusAttribut.PRECISION,basicDBObject.get(ArretBusAttribut.PRECISION));
+				
+				//on modifie la date prévue seulementde notre collection
+				collection.update(searchIfArretBusAlreadyExist, UpdatedBasicDBObject);
+				
+			} else {//on a bien parse notre objet et qu'il n'existe pas deja, on l'insere
+				collection.insert(basicDBObject);
+				System.out.println(basicDBObject);
+			} 
+		} 
+
+		
+		
 		DBCursor cursor = collection.find();
 		while(cursor.hasNext()) {
 			DBObject object = cursor.next();
